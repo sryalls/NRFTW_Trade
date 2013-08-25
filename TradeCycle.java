7154f4a3f7bd01cd0f80242034f81951b67d6eaa
sryalls <sandy.ryalls@googlemail.com>
2013-08-25 05:49:14 -0400

    new DB dummp

--- TradeCycle.java
+++ TradeCycle.java
@@ -30,8 +30,10 @@ public class TradeCycle {
         for(Iterator<Market> aMarketItterator = theMarkets.iterator(); aMarketItterator.hasNext();){
             Market aMarket = aMarketItterator.next();
             //for each commodity
+            System.out.println(aMarket.theName);
             for(Iterator<Commodity> aCommodityItterator = theCommodities.iterator(); aCommodityItterator.hasNext();){
                 Commodity aCommodity = aCommodityItterator.next();
+                System.out.println(aCommodity.theName);
                 String getPRateQuery = "select p_rate, stock from trade_matrix where market_name = '"+aMarket.theName+"' and commodity_name = '"+aCommodity.theName+"'";
                 ResultSet pRateResults = NRFTW_Trade.dBQuery(getPRateQuery);
                 int productionRate = 0;
@@ -58,6 +60,7 @@ public class TradeCycle {
                     System.out.println(ex);
                 } 
                 //for each point of produciton rate
+                System.out.println("Producing");
                 for(int i=1;i<=productionRate;i++)
                 {                
                     //add a unit to the stock pile
@@ -80,28 +83,43 @@ public class TradeCycle {
                 //get consumption rate
                 String getPRateQuery = "select c_rate from trade_matrix where market_name = '"+aMarket.theName+"' and tag_name = '"+aTag.theName+"' and tag_grade = '"+aTag.theGrade+"'";
                 ResultSet pRateResults = NRFTW_Trade.dBQuery(getPRateQuery);
+                System.out.println("consuming: "+aTag.theName+" :1");
                 int consumptionRate = 0;
-                
+                System.out.println("consuming: "+aTag.theName+" :2");
                 try{
                     while(pRateResults.next()){
-                        consumptionRate = pRateResults.getInt(1);                        
+                        System.out.println("consuming: "+aTag.theName+" :3");
+                        consumptionRate = pRateResults.getInt(1);            
+                        System.out.println("consuming: "+aTag.theName+" :4");
                     }
                 }catch(SQLException ex){
                     System.out.println(ex);
                 } 		
+                System.out.println("consuming: "+aTag.theName+" :5");
 		//get each comodity
                     //include any of a higher tag
                 String commodityQuery = "select trade_matrix.commodity_name, trade_matrix.stock, prices.price from trade_matrix inner join prices on trade_matrix.commodity_name = prices.commodity and trade_matrix.market_name = prices.market where trade_matrix.commodity_name in (select commodity from commodities_tags where tag = '"+aTag.theName+"' and grade >= "+aTag.theGrade+") and trade_matrix.market_name = '"+aMarket.theName+"' order by price asc";
+                System.out.println("consuming: "+aTag.theName+" :6");
+                System.out.println(commodityQuery);
                 ResultSet commodityResults = NRFTW_Trade.dBQuery(commodityQuery);
-                 ArrayList theConsumedCommodities = new ArrayList();
+                System.out.println("consuming: "+aTag.theName+" :7");
+                ArrayList theConsumedCommodities = new ArrayList();
+                System.out.println("consuming: "+aTag.theName+" :8");
                 try{                   
                     while(commodityResults.next()){
+                        System.out.println("consuming: "+aTag.theName+" :9");
                         String thisCommodityName = commodityResults.getString(1);
+                        System.out.println("consuming: "+aTag.theName+" :10");
                         int thisCommodityStock = commodityResults.getInt(2);
+                        System.out.println("consuming: "+aTag.theName+" :11");
                         int thisCommodityPrice = commodityResults.getInt(3);
+                        System.out.println("consuming: "+aTag.theName+" :12");
                         Commodity thisCommodity = new Commodity(thisCommodityName,thisCommodityStock,thisCommodityPrice);
+                        System.out.println("consuming: "+aTag.theName+" :13");
                         theConsumedCommodities.add(thisCommodity);
+                        System.out.println("consuming: "+aTag.theName+" :14");
                         Collections.sort(theConsumedCommodities);
+                        System.out.println("consuming: "+aTag.theName+" :15");
         		//rank by local price
                     }                                                                  
                 }catch(SQLException ex){
@@ -109,39 +127,52 @@ public class TradeCycle {
                 } 
                 //loop until consumption rate is satisfied
                 int fulfilledConsumption = 0;
-                
-                while(fulfilledConsumption < consumptionRate){                	
+                System.out.println("consuming: "+aTag.theName+" :16");
+                while(fulfilledConsumption < consumptionRate){  
+                    System.out.println("consuming: "+aTag.theName+" :17");                    
                     //get cheapest commodity
-                    if(theConsumedCommodities.size()>0){
+                    if(theConsumedCommodities.size() > 0){
+                        System.out.println("consuming: "+aTag.theName+" :18");
                         Commodity forConsumption = (Commodity) theConsumedCommodities.get(0);
+                        System.out.println("consuming: "+aTag.theName+" :19");
                             //if commodity has stockpile
                             if(forConsumption.theStockLevel > 0){
                                 //..consume unit of that commodity
                                 if(forConsumption.thePrice == 0){
+                                    System.out.println("consuming: "+aTag.theName+" :20");
                                     forConsumption.thePrice ++;
                                 }
                                 forConsumption.thePrice = (forConsumption.thePrice + ((forConsumption.thePrice/forConsumption.theStockLevel)/2));
                                 //adjust price of commodity
                                 forConsumption.theStockLevel --;
+                                System.out.println("consuming: "+aTag.theName+" :21");
                             }else{
                                 //adjust price of commodity by more
                                 forConsumption.thePrice = (forConsumption.thePrice + (forConsumption.thePrice/2));
+                                System.out.println("consuming: "+aTag.theName+" :22");
                             }
                         //add one to consumption rate satisfaction
                         fulfilledConsumption ++;
                         // resort commodities\
+                        System.out.println("consuming: "+aTag.theName+" :23");
+                    }else{
+                        System.out.println("cannot resolve consumed commodities for '"+aTag.theName+"' in '"+aMarket.theName+"' The database is incomplete. Terminating.");
+                        System.exit(0);
                     }
                 }
             //close tag loop
             }
         //close market loop 
         }
+        System.out.println("Trading");
         //for each market
         for(Iterator<Market> aMarketItterator = theMarkets.iterator(); aMarketItterator.hasNext();){
             Market aMarket = aMarketItterator.next();
             //for each comodity
+            System.out.println(aMarket.theName);
             for(Iterator<Commodity> aCommodityItterator = theCommodities.iterator(); aCommodityItterator.hasNext();){
                 Commodity theTradingCommodity = aCommodityItterator.next();
+                System.out.println(theTradingCommodity.theName);
                 //trade loop (loop until no trade is made)
                 boolean tradeHappens = true; 
                 while(tradeHappens = true){
