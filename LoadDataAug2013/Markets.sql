e1e9f3adcdea00cd590baae354e3de51f8f62df6
sryalls <sandy.ryalls@googlemail.com>
2013-08-25 09:43:00 -0400

    some new test data constructed and loaded

---
+++ LoadDataAug2013/Markets.sql
@@ -0,0 +1,10 @@
+LOCK TABLES `markets` WRITE;
+/*!40000 ALTER TABLE `markets` DISABLE KEYS */;
+INSERT INTO `markets` VALUES
+('Schattenberg'),
+('Chelm'),
+('Serap'),
+('Krypte'),
+('Varyaga'),
+('Arboria');
+UNLOCK TABLES;
